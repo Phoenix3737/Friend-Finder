6e9fbd45423746f4d65ffdf5a7991995fc562255
phoenix3737 <groves999@gmail.com>
2018-04-19 19:12:51 -0400

    intitial commit

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+
+
+var friends = [ {
+    "name" : "Tony",
+    "photo" : "https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/paramount-35065-Full-Image_GalleryBackground-en-US-1483994508505._RI_SX940_.jpg",
+    "scores" : [3,2,1,5,2,3,5,3,1,5]
+}, {
+    "name" : "Bruce",
+    "photo" : "https://images.forbes.com/media/2013/07/16/0716_bruce-wayne_416x416.jpg",
+    "scores" : [1,5,5,5,3,1,2,3,3,5]
+}, {
+    "name" : "Barbara",
+    "photo" : "https://vignette.wikia.nocookie.net/marvelcrossroads/images/4/44/YUBatgirl.jpg/revision/latest?cb=20160513021511",
+    "scores" : [1,3,4,4,5,5,4,1,1,1]
+}, {
+    "name" : "Selina",
+    "photo" : "https://cdn.europosters.eu/image/750/posters/batman-dark-knight-rises-catwoman-i12732.jpg",
+    "scores" : [3,3,3,4,5,1,2,3,5,4]
+}];
+
+module.exports = friends;
+
+
+    
